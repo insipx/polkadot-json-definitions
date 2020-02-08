a3dc725722de924c634569f17ff493be66f3ff36
Andrew Plaza <aplaza@liquidthink.net>
2020-02-08 20:06:31 +0100

    qol changes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git submodule init
+git submodule update
 npm install --save typescript
 npm install --save @polkadot/api
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import * as definitions from "./dot-api/packages/types/src/interfaces/definitions";
-import * as overrides from "./dot-api/packages/types/src/known/overrides";
+import * as Definitions from "./dot-api/packages/types/src/interfaces/definitions";
+import * as Overrides from "./dot-api/packages/types/src/known/overrides";
+import fs from 'fs';
 
 // console.log(definitions);
-console.log(JSON.stringify(definitions));
-console.log(JSON.stringify(overrides));
+const defs = JSON.stringify(Definitions, null, 2);
+const overrides = JSON.stringify(Overrides, null, 2);
+
+fs.writeFileSync("definitions.json", defs);
+fs.writeFileSync("overrides.json", overrides);
